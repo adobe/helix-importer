ba090ede97b680c9e550d1d6038c0a0bab7cd646
Alexandre Capt <acapt@adobe.com>
2022-01-19 16:39:58 +0100

    chore: clean lint

--- src/explorer/PagingExplorer.js
+++ src/explorer/PagingExplorer.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* eslint-disable class-methods-use-this */
+/* eslint-disable class-methods-use-this, no-console */
 
 import { JSDOM } from 'jsdom';
 
