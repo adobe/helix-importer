ec8f57afe7e3ce023eec593ad962b8189d093172
Tobias Bocanegra <tripod@adobe.com>
2023-01-19 14:56:01 +0100

    chore: remove commented line

--- src/importer/hast-to-mdast-gridtable-handlers.js
+++ src/importer/hast-to-mdast-gridtable-handlers.js
@@ -9,7 +9,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-// import { all } from 'hast-util-to-mdast';
 import {
   TYPE_BODY,
   TYPE_CELL,
